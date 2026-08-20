5d235ce090dee32e9451a9a3e7e861679731fbc0
hmtnvac-cpu <hmtnvac@gmail.com>
2026-08-20 08:09:11 +0700

    Create channels.js

---
+++ channels.js
@@ -0,0 +1,390 @@
+const uniq = arr =>
+  [...new Set(arr.map(x => String(x).trim()).filter(Boolean))];
+
+function makeChannel(id, group, name, logo, streams) {
+  return {
+    id,
+    type: "tv",
+    group,
+    name,
+    logo,
+    streams: uniq(streams)
+  };
+}
+
+const LOGOS = {
+  cbs:
+    "https://raw.githubusercontent.com/tv-logo/tv-logos/main/countries/united-states/cbs-sports-network-us.png",
+
+  nbc:
+    "https://raw.githubusercontent.com/tv-logo/tv-logos/main/countries/united-states/nbc-sports-us.png",
+
+  now:
+    "https://www.tvlogo.org/hong-kong/now-sports-prime-hk.png",
+
+  skyMain:
+    "https://www.tvlogo.org/united-kingdom/sky-sports-main-event-uk.png",
+
+  skyPL:
+    "https://www.tvlogo.org/united-kingdom/sky-sports-premier-league-uk.png",
+
+  sportsnet:
+    "https://raw.githubusercontent.com/tv-logo/tv-logos/main/countries/canada/sportsnet-sn1-ca.png",
+
+  usa:
+    "https://raw.githubusercontent.com/tv-logo/tv-logos/main/countries/united-states/usa-us.png",
+
+  universo:
+    "https://raw.githubusercontent.com/tv-logo/tv-logos/main/countries/united-states/universo-us.png",
+
+  bein:
+    "https://www.tvlogo.org/france/bein-sports-fr.png",
+
+  digi:
+    "https://www.tvlogo.org/romania/digi-sport-1-ro.png",
+
+  eleven:
+    "https://tvlogo.org/belgium/eleven-sports-1-fr-be.png",
+
+  sky:
+    "https://www.tvlogo.org/united-kingdom/sky-sports-icon-uk.png",
+
+  skyDarts:
+    "https://www.tvlogo.org/united-kingdom/sky-sports-darts-uk.png",
+
+  skyF1:
+    "https://www.tvlogo.org/united-kingdom/sky-sports-f1-uk.png",
+
+  tf1:
+    "https://www.tvlogo.org/france/tf1-fr.png",
+
+  tnt:
+    "https://www.tvlogo.org/united-kingdom/tnt-sports.png",
+
+  tntUltimate:
+    "https://pixvid.org/images/2025/01/16/TNT-Sports-Ultimate.png",
+
+  vsport:
+    "https://res.cloudinary.com/dnaoyj/image/upload/dpr_2,f_auto,q_auto,w_600/v1/Assets/KLT/DNA%20TV/Kanavapaketit/ohjelmakirjastot/V%20kanavat/v_sport_suomi__240X240"
+};
+
+const channels = [
+
+  // ================= VTV =================
+
+  makeChannel("vtv1","vtv","VTV1",null,[
+    "https://vips-livecdn.fptplay.net/live/media/vtv1/live247-hls-avc/vtv1-avc1_5600000=10000-mp4a_131600=20000.m3u8"
+  ]),
+
+  makeChannel("vtv2","vtv","VTV2",null,[
+    "https://vips-livecdn.fptplay.net/live/media/vtv2/live247-hls-avc/vtv2-avc1_5600000=10000-mp4a_131600=20000.m3u8"
+  ]),
+
+  makeChannel("vtv3","vtv","VTV3",null,[
+    "https://vips-livecdn.fptplay.net/live/media/vtv3/live247-hls-avc/vtv3-avc1_5600000=10000-mp4a_131600=20000.m3u8"
+  ]),
+
+  makeChannel("vtv4","vtv","VTV4",null,[
+    "https://vips-livecdn.fptplay.net/live/media/vtv4/live247-hls-avc/vtv4-avc1_5600000=10000-mp4a_131600=20000.m3u8"
+  ]),
+
+  makeChannel("vtv5","vtv","VTV5",null,[
+    "https://vips-livecdn.fptplay.net/live/media/vtv5/live247-hls-avc/vtv5-avc1_5600000=10000-mp4a_131600=20000.m3u8"
+  ]),
+
+  makeChannel("vtv6","vtv","VTV6",null,[
+    "https://vips-livecdn.fptplay.net/live/media/vtv6/live247-hls-avc/vtv6-avc1_5600000=10000-mp4a_131600=20000.m3u8"
+  ]),
+
+  makeChannel("vtv7","vtv","VTV7",null,[
+    "https://vips-livecdn.fptplay.net/live/media/vtv7/live247-hls-avc/vtv7-avc1_5600000=10000-mp4a_131600=20000.m3u8"
+  ]),
+
+  makeChannel("vtv8","vtv","VTV8",null,[
+    "https://vips-livecdn.fptplay.net/live/media/vtv8/live-hls-avc/vtv8-avc1_4000000=10000-mp4a_131600=20000.m3u8"
+  ]),
+
+  makeChannel("vtv9","vtv","VTV9",null,[
+    "https://vips-livecdn.fptplay.net/live/media/vtv9/live247-hls-avc/vtv9-avc1_5600000=10000-mp4a_131600=20000.m3u8"
+  ]),
+
+  makeChannel("vtv10","vtv","VTV10",null,[
+    "https://vips-livecdn.fptplay.net/live/media/vtv10/live247-hls-avc/vtv10-avc1_5600000=10000-mp4a_131600=20000.m3u8"
+  ]),
+
+  // ================= 1080 =================
+
+  makeChannel(
+    "cbs-sports-1080",
+    "sports1080",
+    "CBS Sports 1080p 60 FPS",
+    LOGOS.cbs,
+    [
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:e8:95:b7&stream=45601&extension=ts&play_token=FzL6BKqvEe",
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:7d:69:87&stream=1931140&extension=ts&play_token=78QsJViQ2M",
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7E:19:50&stream=1931140&extension=ts&play_token=drj6UiUd3H",
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7F:A7:54&stream=1931140&extension=ts&play_token=dMBw5KtrCr"
+    ]
+  ),
+
+  makeChannel(
+    "nbc-sports-1080",
+    "sports1080",
+    "NBC Sports 1080p 60 FPS",
+    LOGOS.nbc,
+    [
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:e8:95:b7&stream=1124350&extension=ts&play_token=Nye7KFsDtT",
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:7d:69:87&stream=1124350&extension=ts&play_token=eNXy4IgwMX",
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7E:19:50&stream=1124350&extension=ts&play_token=7VZFU5RGrb",
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7E:19:50&stream=234677&extension=ts&play_token=tvycd3S4G4",
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7F:A7:54&stream=1124350&extension=ts&play_token=rQKi6Aw8Jy"
+    ]
+  ),
+
+  makeChannel(
+    "now-sports-1080",
+    "sports1080",
+    "NOW Sports 1080p 60 FPS",
+    LOGOS.now,
+    [
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:e8:95:b7&stream=1948655&extension=ts&play_token=tbb2RAOWTW",
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:10:01:3e&stream=1948655&extension=ts&play_token=wKjCMLCsCQ",
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:10:01:3e&stream=1948655&extension=ts&play_token=4dOHhrrZQ5",
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7E:19:50&stream=1948655&extension=ts&play_token=kDi24LfUfG",
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7F:A7:54&stream=1948655&extension=ts&play_token=0MqG6oZhXz"
+    ]
+  ),
+
+  makeChannel(
+    "sky-sports-main-event-fhd",
+    "sports1080",
+    "Sky Sports Main Event FHD",
+    LOGOS.skyMain,
+    [
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:10:01:3e&stream=1905853&extension=ts&play_token=VcLSkUIKoV",
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:7d:69:87&stream=1905853&extension=ts&play_token=HJKEBcMdYW",
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7E:19:50&stream=1905853&extension=ts&play_token=tFtdcozl0Z",
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7F:A7:54&stream=1905853&extension=ts&play_token=0PellAUHuM"
+    ]
+  ),
+
+  makeChannel(
+    "sky-sports-premier-league-fhd",
+    "sports1080",
+    "Sky Sports Premier League FHD",
+    LOGOS.skyPL,
+    [
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7F:A7:54&stream=1905844&extension=ts&play_token=HE8bJPpkM0",
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:10:01:3e&stream=1905844&extension=ts&play_token=AIylOstlvH",
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:7d:69:87&stream=1905844&extension=ts&play_token=VauO51P7Uz",
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7E:19:50&stream=1905844&extension=ts&play_token=Rp6htQTP1z"
+    ]
+  ),
+
+  makeChannel(
+    "sportsnet-one-ca-1080",
+    "sports1080",
+    "Sportsnet One CA 1080p 60 FPS",
+    LOGOS.sportsnet,
+    [
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:e8:95:b7&stream=1948644&extension=ts&play_token=67dYXdFMD5",
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7E:19:50&stream=1948650&extension=ts&play_token=IeulQneT0e",
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7F:A7:54&stream=1948650&extension=ts&play_token=kOpzJPZpl8"
+    ]
+  ),
+
+  makeChannel(
+    "usa-network-1080",
+    "sports1080",
+    "USA Network 1080p 60 FPS",
+    LOGOS.usa,
+    [
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:10:01:3e&stream=1930887&extension=ts&play_token=17642b7BAL",
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:10:01:3e&stream=45466&extension=ts&play_token=BhBlcThs4o",
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:7d:69:87&stream=1930887&extension=ts&play_token=wXIyU8E7YD",
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7E:19:50&stream=1930887&extension=ts&play_token=J2KdNN5TYM",
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7F:A7:54&stream=1930887&extension=ts&play_token=tdDzhWuRot"
+    ]
+  ),
+
+  makeChannel(
+    "universo-1080",
+    "sports1080",
+    "Universo 1080p 60 FPS",
+    LOGOS.universo,
+    [
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:10:01:3e&stream=1930892&extension=ts&play_token=Qu3YNaQE54",
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:7d:69:87&stream=1930892&extension=ts&play_token=EaKf87ZAR8",
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7E:19:50&stream=1930892&extension=ts&play_token=Dshjs6wiNd"
+    ]
+  ),
+
+  // ================= UHD / 4K =================
+
+  makeChannel(
+    "bein-sports-uhd",
+    "sports4k",
+    "beIN Sports UHD",
+    LOGOS.bein,
+    [
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:e8:95:b7&stream=1948627&extension=ts&play_token=Vlmr36mT65",
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:10:01:3e&stream=1948627&extension=ts&play_token=ZylqK6Jlon",
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7F:A7:54&stream=1948627&extension=ts&play_token=5fIeli4ylw"
+    ]
+  ),
+
+  makeChannel(
+    "digi-sport-uhd",
+    "sports4k",
+    "Digi Sport UHD",
+    LOGOS.digi,
+    [
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:10:01:3e&stream=1632122&extension=ts&play_token=SFUMHAeisS",
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:e8:95:b7&stream=1632122&extension=ts&play_token=SpK901Rc9Y",
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7F:A7:54&stream=1632122&extension=ts&play_token=1Fi4sgyPhW"
+    ]
+  ),
+
+  makeChannel(
+    "eleven-sports-1-uhd",
+    "sports4k",
+    "Eleven Sports 1 UHD",
+    LOGOS.eleven,
+    [
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:e8:95:b7&stream=1470618&extension=ts&play_token=e0V5DEpkoI",
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:e8:95:b7&stream=1948660&extension=ts&play_token=PkLWuD6skY",
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:10:01:3e&stream=1470618&extension=ts&play_token=z0mDj0DrdW",
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7F:A7:54&stream=1948660&extension=ts&play_token=EIr5yxfGPr"
+    ]
+  ),
+
+  makeChannel(
+    "sky-sports-1-uhd",
+    "sports4k",
+    "Sky Sports 1 UHD",
+    LOGOS.sky,
+    [
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:e8:95:b7&stream=1608068&extension=ts&play_token=ChIhpB8guR",
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:10:01:3e&stream=1608068&extension=ts&play_token=bNqN7HTIKX",
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7F:A7:54&stream=1608068&extension=ts&play_token=djxHx6Uy9j"
+    ]
+  ),
+
+  makeChannel(
+    "sky-sports-2-uhd",
+    "sports4k",
+    "Sky Sports 2 UHD",
+    LOGOS.sky,
+    [
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:e8:95:b7&stream=1608069&extension=ts&play_token=boMozScv7z",
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:10:01:3e&stream=1608069&extension=ts&play_token=Jn8XYQ8soh",
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7F:A7:54&stream=1608069&extension=ts&play_token=T7pDKwjV1Z"
+    ]
+  ),
+
+  makeChannel(
+    "sky-sports-bundesliga-uhd",
+    "sports4k",
+    "Sky Sports Bundesliga UHD",
+    LOGOS.sky,
+    [
+      "http://mag.tivi-one-iptv.net:80/play/live.php?mac=00:1A:79:0E:0F:8E&stream=893917&extension=ts&play_token=5QPOI5t6D3"
+    ]
+  ),
+
+  makeChannel(
+    "sky-sports-darts-uhd",
+    "sports4k",
+    "Sky Sports Darts UHD",
+    LOGOS.skyDarts,
+    [
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:e8:95:b7&stream=1471382&extension=ts&play_token=Xjz7IglxZB",
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:10:01:3e&stream=1471382&extension=ts&play_token=FqtO3KWqUT",
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7F:A7:54&stream=1471382&extension=ts&play_token=4wKKTKtM8L"
+    ]
+  ),
+
+  makeChannel(
+    "sky-sports-f1-uhd",
+    "sports4k",
+    "Sky Sports F1 UHD",
+    LOGOS.skyF1,
+    [
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:10:01:3e&stream=1761500&extension=ts&play_token=3TLQcXqTKG",
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7F:A7:54&stream=1761500&extension=ts&play_token=OWv81jhV3e"
+    ]
+  ),
+
+  makeChannel(
+    "sky-sports-main-event",
+    "sports4k",
+    "Sky Sports Main Event",
+    LOGOS.skyMain,
+    [
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:e8:95:b7&stream=1608071&extension=ts&play_token=BZa17svpdY",
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:10:01:3e&stream=1608071&extension=ts&play_token=YPIwjxNGfQ",
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7F:A7:54&stream=1641636&extension=ts&play_token=Oxc5PyW4yK"
+    ]
+  ),
+
+  makeChannel(
+    "sky-sports-uhd",
+    "sports4k",
+    "Sky Sports UHD",
+    LOGOS.sky,
+    [
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:e8:95:b7&stream=1471387&extension=ts&play_token=54v3D6UwQT",
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:e8:95:b7&stream=1753227&extension=ts&play_token=3Ea6Hxt5oq",
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7F:A7:54&stream=1471387&extension=ts&play_token=Bx3mlhSM6x"
+    ]
+  ),
+
+  makeChannel(
+    "tf1-hdr-uhd",
+    "sports4k",
+    "TF1 HDR UHD",
+    LOGOS.tf1,
+    [
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:10:01:3e&stream=1640379&extension=ts&play_token=YWsJoaBWoi",
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:e8:95:b7&stream=1640379&extension=ts&play_token=6gj5Htj59g",
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7F:A7:54&stream=1640379&extension=ts&play_token=qXUKertYkr"
+    ]
+  ),
+
+  makeChannel(
+    "tnt-sports-uhd",
+    "sports4k",
+    "TNT Sports UHD",
+    LOGOS.tnt,
+    [
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:e8:95:b7&stream=1479591&extension=ts&play_token=vzYU4fDUAQ",
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:10:01:3e&stream=1479591&extension=ts&play_token=9JzXAViV1F",
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7F:A7:54&stream=1479591&extension=ts&play_token=mSsCW5SfPM"
+    ]
+  ),
+
+  makeChannel(
+    "tnt-sports-ultimate-uhd",
+    "sports4k",
+    "TNT Sports Ultimate UHD",
+    LOGOS.tntUltimate,
+    [
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:e8:95:b7&stream=1595637&extension=ts&play_token=YdoiZErSQZ"
+    ]
+  ),
+
+  makeChannel(
+    "v-sport-plus-uhd",
+    "sports4k",
+    "V Sport+ UHD",
+    LOGOS.vsport,
+    [
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:e8:95:b7&stream=1632123&extension=ts&play_token=Aeer3FFu7o",
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:e8:95:b7&stream=1749221&extension=ts&play_token=jg5ZtLtCQN",
+      "http://line.moja-teve9.me:80/play/live.php?mac=00:1A:79:10:01:3e&stream=1632123&extension=ts&play_token=E6ziX5leBT",
+      "http://chaotic-streams.cc:80/play/live.php?mac=00:1A:79:7F:A7:54&stream=1632123&extension=ts&play_token=6GO8Kiqjfm"
+    ]
+  )
+];
+
+module.exports = { channels };
